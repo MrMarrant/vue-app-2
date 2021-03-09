e35fbfe53c522a522669f78f45312a264d488109
Georges <georgesluis.tocquec@epsi.fr>
2021-03-09 14:00:00 +0100

    Changement majeur avec vue dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,17 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Test from '../views/Test.vue'
+import VueRouter from "vue-router";
+import routes from "./routes";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/test',
-    name: 'Test',
-    component: Test
-  }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
-
+// configure router
 const router = new VueRouter({
-  routes
-})
+  routes, // short for routes: routes
+  linkExactActiveClass: "active",
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return {selector: to.hash}
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+});
 
-export default router
+export default router;
